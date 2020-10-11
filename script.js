744398f9b24b964b1ce8704c79bd74bb99159037
Gustavo Cesário <gs.cesario1998@gmail.com>
2020-10-10 22:50:16 -0300

    ainda está cedo...

---
+++ script.js
@@ -0,0 +1,24 @@
+setInterval(relogio, 1000);
+
+function relogio(){
+    const horaAtual = gerarHora();
+    const texto = `${horaAtual.hora}:${horaAtual.minuto}:${horaAtual.segundo}`
+    document.getElementById("hora").innerHTML = texto
+}
+
+function gerarHora(){
+    const horaAtual = new Date();
+    const hora = horaAtual.getHours()
+    const minuto = horaAtual.getMinutes()
+    const segundo = horaAtual.getSeconds()
+
+    if(hora < 10) hora = `0${hora}`
+    if(minuto < 10) minuto = `0${minuto}`
+    if(segundo < 10) segundo = `0${segundo}`
+
+    return {
+        hora,
+        minuto,
+        segundo
+    }
+}
