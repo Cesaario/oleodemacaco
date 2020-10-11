52f5591a8e71cfeb486f0242770f11268bb5b731
Gustavo Cesário <gs.cesario1998@gmail.com>
2020-10-11 16:48:44 -0300

    está chegando a hora...

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 setInterval(relogio, 1000);
 
-function relogio(){
+function relogio() {
     const horaAtual = gerarHora();
-    const texto = `${horaAtual.hora}:${horaAtual.minuto}:${horaAtual.segundo}`
-    document.getElementById("hora").innerHTML = texto
+    const textoHorario = `${horaAtual.hora}:${horaAtual.minuto}:${horaAtual.segundo}`
+    const dados = gerarDados();
+    document.getElementById("hora").innerHTML = `${textoHorario}`
+    document.getElementById("texto").innerHTML = `${dados.texto.join("<br>")}`
+    document.getElementById("imagem").src = `${dados.imagem}`
+    document.getElementById("texto").style.fontSize = `${!!dados.fonte ? dados.fonte : "48px"}`
 }
 
-function gerarHora(){
+function gerarHora() {
     const horaAtual = new Date();
-    const hora = horaAtual.getHours()
-    const minuto = horaAtual.getMinutes()
-    const segundo = horaAtual.getSeconds()
+    let hora = horaAtual.getHours()
+    let minuto = horaAtual.getMinutes()
+    let segundo = horaAtual.getSeconds()
 
-    if(hora < 10) hora = `0${hora}`
-    if(minuto < 10) minuto = `0${minuto}`
-    if(segundo < 10) segundo = `0${segundo}`
+    if (hora < 10) hora = `0${hora}`
+    if (minuto < 10) minuto = `0${minuto}`
+    if (segundo < 10) segundo = `0${segundo}`
+
+    if (true) {
+        hora = "23"
+        minuto = "05"
+    }
 
     return {
         hora,
@@ -23,11 +32,44 @@ function gerarHora(){
     }
 }
 
-function gerarTexto(){
-    const hora = gerarHora();
-    return [
-        "Está cedo demais",
-        "para o óleo",
-        "de macaco."
-    ]
+function gerarDados() {
+    const horaAtual = gerarHora();
+    if (parseInt(horaAtual.hora) == 0) {
+        return {
+            texto: [
+                "MEIA NOITE!!!",
+                "HORÁRIO OFICIAL DO",
+                "ÓLEO",
+                "DE",
+                "MACACO"
+            ],
+            imagem: 'imagens/Macaco-da-noite.jpg',
+            fonte: '64px'
+        }
+    } else if (parseInt(horaAtual.hora) == 1) {
+        return {
+            texto: [
+                "VOCÊ CHEGOU TARDE DEMAIS",
+                "PARA O ÓLEO",
+                "DE MACACO"
+            ],
+            imagem: 'imagens/macaco-da-noite-especie.jpg'
+        }
+    } else if (parseInt(horaAtual.hora) < 23) {
+        return {
+            texto: [
+                "ESTÁ CEDO DEMAIS",
+                "PARA O ÓLEO",
+                "DE MACACO"
+            ],
+            imagem: 'imagens/macaco-pequeno.jpg'
+        }
+    } else if (parseInt(horaAtual.hora) == 23) {
+        return {
+            texto: [
+                "FALTA UMA HORA..."
+            ],
+            imagem: 'imagens/unnamed.jpg'
+        }
+    }
 }
